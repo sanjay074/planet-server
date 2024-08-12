04af91b1e6aa914c640ecedc8da9bc2dc1e129e0
Durgesh Gaur <durgesh@uveoustech.com>
2024-08-12 17:56:35 +0530

    add 404 middleware and add the new code

--- server.js
+++ server.js
@@ -5,16 +5,20 @@ require("dotenv").config();
 require("./db/connect");
 const Router = require("./routes/rootRouter");
 const { default: helmet } = require("helmet");
-app.use((req, res, next) => {
-  res.status(404).json({status:false, message:'URL not found'});
-});
+
 app.use(helmet());
 app.use(express.json());
 app.use(cors());
 app.use(express.static("public"));
 app.use("/public", express.static("public"));
 app.use("/api/v1", Router);
-
+app.use((req, res, next) => {
+  res.status(404).json({
+      status:false,
+      error: 'API URL not found',
+      message: `The requested URL ${req.originalUrl} was not found on this server.`,
+  });
+});
 app.get("/", (req, res) => {
   res.send("hello planet");
 });
