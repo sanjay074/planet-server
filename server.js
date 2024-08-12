3eb97c00133694c726191c82132bbb0baa6f1845
ankit gupta <103044611+ankitg94@users.noreply.github.com>
2024-08-12 18:10:42 +0530

    server file addded

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ require("dotenv").config();
 require("./db/connect");
 const Router = require("./routes/rootRouter");
 const { default: helmet } = require("helmet");
-app.use((req, res, next) => {
-  res.status(404).json({status:false, message:'URL not found'});
-});
+
+// app.use((req, res, next) => {
+//   res.status(404).json({status:false, message:'URL not found'});
+// });
 app.use(helmet());
 app.use(express.json());
 app.use(cors());
