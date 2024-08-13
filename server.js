05bc71a235abb8fdddb67db5a9989dbc0ac6839e
ankit gupta <103044611+ankitg94@users.noreply.github.com>
2024-08-13 10:41:36 +0530

    add new code

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ require("./db/connect");
 const Router = require("./routes/rootRouter");
 const { default: helmet } = require("helmet");
 
-// app.use((req, res, next) => {
-//   res.status(404).json({status:false, message:'URL not found'});
-// });
+
 app.use(helmet());
 app.use(express.json());
 app.use(cors());
@@ -19,7 +17,7 @@ app.use("/api/v1", Router);
 app.get("/", (req, res) => {
   res.send("hello planet");
 });
-const Port = process.env.PORT||4000
+const Port = process.env.PORT||3000
 
 app.listen(process.env.PORT,() => {
   console.log(`Server running on port ${Port}`);
