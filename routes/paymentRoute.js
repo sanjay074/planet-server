1b1e3c4cd6d92df040cc21ee69e707b6b17576cd
Durgesh Gaur <durgesh@uveoustech.com>
2024-08-21 17:40:55 +0530

    add payment api

--- routes/paymentRoute.js
+++ routes/paymentRoute.js
@@ -1,5 +1,5 @@
 const express =require("express")
-const {paymentType,getAllPaymentType,deletePaymentType,getPaymentType,generateUpiQrcode, paymentHistory}=require("../controllers/paymentController");
+const {paymentType,getAllPaymentType,deletePaymentType,getPaymentType,generateUpiQrcode, paymentHistory,validateQrcode}=require("../controllers/paymentController");
 const {authMiddleware,isAdminMd}  =require("../middleware/authMiddle")
 const {upload} = require("../middleware/singleFileUpload")
 const paymentRoute =express.Router()
@@ -8,5 +8,6 @@ paymentRoute.get("/getAllPaymentType",authMiddleware,isAdminMd,getAllPaymentType
 paymentRoute.delete("/remove/:id",authMiddleware,isAdminMd,deletePaymentType);
 paymentRoute.get("/get/:id",authMiddleware,isAdminMd,getPaymentType);
 paymentRoute.post("/generateUpiQrcode",generateUpiQrcode);
-paymentRoute.post("/utrVerify",authMiddleware,paymentHistory)
+paymentRoute.post("/utrVerify",authMiddleware,paymentHistory);
+paymentRoute.post("/validateQr",validateQrcode);
 module.exports =paymentRoute ;
